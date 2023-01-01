79a0176565b791e6a57f81b319f46fdc2db971f7
borebrian2021 <bkimutai2021@gmail.com>
2023-01-01 11:25:54 +0300

    Completted projects section.

--- src/components/BodySection.jsx
+++ src/components/BodySection.jsx
@@ -102,72 +102,24 @@ const BodySection = () => {
               passionate, outspoken, multi-talented individual that learns
               quickly.{" "}
             </p>
+            <button className="btn gap-2 m-5 btn-sm text-xs ">
+                Preview CV
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
 
+              </button>
             <div className="w-[100%] flex  m-[3px] justify-center ">
               <div className="w-[80%] text-white flex grid-col-5 h-[70px] m-[3px]">
-                <div className="w-[30px] h-[30px] rounded-full m-3  m-[0.2%] bg-[#222228] text-white flex justify-center align-center items-center">
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                      class="w-5 h-5"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M94.12 315.1c0 25.9-21.16 47.06-47.06 47.06S0 341 0 315.1c0-25.9 21.16-47.06 47.06-47.06h47.06v47.06zm23.72 0c0-25.9 21.16-47.06 47.06-47.06s47.06 21.16 47.06 47.06v117.84c0 25.9-21.16 47.06-47.06 47.06s-47.06-21.16-47.06-47.06V315.1zm47.06-188.98c-25.9 0-47.06-21.16-47.06-47.06S139 32 164.9 32s47.06 21.16 47.06 47.06v47.06H164.9zm0 23.72c25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06H47.06C21.16 243.96 0 222.8 0 196.9s21.16-47.06 47.06-47.06H164.9zm188.98 47.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06h-47.06V196.9zm-23.72 0c0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06V79.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06V196.9zM283.1 385.88c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06v-47.06h47.06zm0-23.72c-25.9 0-47.06-21.16-47.06-47.06 0-25.9 21.16-47.06 47.06-47.06h117.84c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06H283.1z"
-                      />
-                    </svg>
-                  </a>
-                </div>
-                <div className="w-[30px] h-[30px] rounded-full m-3  m-[0.2%] bg-[#222228] text-white flex justify-center align-center items-center">
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                      class="w-5 h-5"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
-                      />
-                    </svg>
-                  </a>
-                </div>{" "}
-                <div className="w-[30px] h-[30px] rounded-full m-3  m-[0.2%] bg-[#222228] text-white flex justify-center align-center items-center">
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                      class="w-5 h-5"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                      />
-                    </svg>
-                  </a>
-                </div>{" "}
-                <div className="w-[30px] h-[30px] rounded-full m-3  m-[0.2%] bg-[#222228] text-white flex justify-center align-center items-center">
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 496 512"
-                      class="w-5 h-5"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                      />
-                    </svg>
-                  </a>
-                </div>
+               
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className="h-[100%] w-[100%] flex justify-center col-span-3 items-center align-center">
-        <div className=" rounded-md  bg-[#31313a] w-[100%] gap-1 mr-3 h-[95%] ml-3">
+        <div className=" rounded-md  bg-[#31313a] w-[100%] overflow-x-auto overflow-y-auto gap-1 mr-3 h-[95%] ml-3">
           <div className="tabs flex w-[100%] justify-center items-center align-center mt-5">
             <a className={about ? "tab tab-active tab-lifted " : "tab text-white"} onClick={() => { setActiveTab(1) }} >About me</a>
             <a className={education ? "tab tab-active tab-lifted " : "tab text-white"} onClick={() => { setActiveTab(2) }}>Certification</a>
@@ -176,9 +128,9 @@ const BodySection = () => {
             <a className={testimonials ? "tab tab-active tab-lifted " : "tab text-white"} onClick={() => { setActiveTab(5) }}>Testimonials</a>
           </div>
           {
-            about ? <div className="w-[100%] p-[20px]  text-center justify-center flex  flex-col-reverse align-center items-center ">
-              <button className="btn gap-2 m-5 ">
-                Preview CV
+            about ? <div className="w-[100%] p-[20px]   text-center justify-center flex  flex-col-reverse align-center items-center ">
+              <button className="btn gap-2 m-5 btn-sm text-xs ">
+                Hire me
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -193,6 +145,7 @@ const BodySection = () => {
                 energetic in user experience design, interfaces and web
                 development.
               </p>
+            
               <div className=" grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-2 grid-col-2 w-[100%]">
                 <div className="w-[100%] grid lg:grid-cols-2 md:grid-col-2 sm:grid-col-2 grid-col-2">
                   <div className="text-left flex atems-left">  <svg
@@ -226,7 +179,7 @@ const BodySection = () => {
             </div>
           }
           {
-            education ? <div className="w-[100%] p-[20px]  text-center justify-center flex  flex-col-reverse align-center items-center ">
+            education ? <div className="w-[100%] p-[20px]  text-center justify-center over flex  flex-col-reverse align-center items-center ">
               <div className=" ">
                 <div className="w-[100%] grid lg:grid-cols-2 md:grid-col-2 sm:grid-col-2 grid-col-2">
 
@@ -376,7 +329,8 @@ const BodySection = () => {
                                       Expert
                                     </div>
                                   </div>
-                                </li> <li class="py-3 sm:py-4">
+                                </li>
+                                 <li class="py-3 sm:py-4">
                                   <div class="flex items-center border-none">
                                     <div class="flex-shrink-0">
                                       <img class="w-8 h-8 " src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1672396319/Portfolio/Tailwind_CSS_Logo.svg_mnon60.png" alt="Neil image" />
@@ -427,6 +381,61 @@ const BodySection = () => {
                               </ul>
                             </div>
                           </div>
+                          <div class="w-[100%] p-4 mt-4 bg-[#222228]  rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                            <div class="flex items-center justify-between mb-4">
+                              <h5 class="text-xl font-bold leading-none text-white dark:text-white">Soft skills</h5>
+                             
+                            </div>
+                            <div class="flow-root">
+                              <ul role="list" class="  ">
+                                <li class="py-3 sm:py-4">
+                                  <div class="flex items-center space-x-4">
+                                  <p class="text-sm font-medium truncate text-white">
+                                        Communication skills
+                                      </p>
+                                   
+                                  </div>
+                                </li>
+                                <li class="py-3 sm:py-4">
+                                  <div class="flex items-center border-none">
+                                    <div class="flex-shrink-0">
+                                    <p class="text-sm font-medium truncate text-white">
+                                       Self Confidence 
+                                    </p>
+                                    </div>                                   
+                                  </div>
+                                </li>
+                                 <li class="py-3 sm:py-4">
+                                  <div class="flex items-center border-none">
+                                    <div class="flex-shrink-0">
+                                    <p class="text-sm font-medium truncate text-white">
+                                        Communication skills
+                                    </p>
+                                        </div>
+                                    <div class="flex-1 min-w-0">
+                                      <p class="text-sm font-medium  text-white">
+                                      </p>
+                                    </div>
+                                  
+                                  </div>
+                                </li> 
+                                <li class="py-3 sm:py-4">
+                                  <div class="flex items-center border-none">
+                                    <div class="flex-shrink-0">
+                                    Leading self
+                                     </div>
+                                    <div class="flex-1 min-w-0">
+                                    
+                                    </div>
+                                    
+
+                                  </div>
+                                </li>
+                                
+
+                              </ul>
+                            </div>
+                          </div>
 
                         </div></div>
                     </section>
@@ -440,7 +449,78 @@ const BodySection = () => {
             </div> : <div>
             </div>
           }
+          
+          {
+            projects ? <div className="w-[100%] p-[20px]  text-center justify-center flex  flex-col-reverse align-center items-center ">
+              <div className="w-[100%] ">
+                <div className="w-[100%] ">
 
+                  <link href="https://unpkg.com/pattern.css" rel="stylesheet" />
+
+                  <div>
+                    <section class="text-gray-200 w-[100%]">
+                      <div class="w-[100%] ">
+                      <div class="lg:flex mb-4 m-10 rounded mx-auto">
+
+<div class="w-full lg:w-1/3 rounded overflow-hidden shadow-lg m-10 relative flex flex-col">
+  <img class="w-full" src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1672560672/Portfolio/Capture_ed9ftc.jpg" alt="Sunset in the mountains"/>
+    <div class="px-6 py-4 h-full">
+      <div class="font-bold text-xl mb-2">Cedar Gardens and Resort</div>
+      <p class="text-white text-base">
+      Complete website demonstrating the services offered at Cedar Gardens and resort.
+      </p>
+    </div>
+  <a href="https://cedargardenhotel.co.ke/" class="w-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+    <span>Visit</span>
+  </a>
+</div>
+
+<div class="w-full lg:w-1/3 rounded overflow-hidden shadow-lg m-10">
+  <img class="w-full" src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1672560672/Portfolio/Capture2_rvr5ht.jpg" alt="Sunset in the mountains"/>
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">Complete Point of Sale</div>
+      <p class="text-gray-700 text-base">
+     Comprehensive web-based point of sale system with integrated thermal printers and scanners. 
+ </p>
+    </div>
+  <a href="https://supremesai.co.ke/log_in/Log_in" class="w-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+    <span>Load Site</span>
+  </a>
+</div>
+
+<div class="w-full lg:w-1/3 rounded overflow-hidden shadow-lg m-10">
+  <img class="w-full" src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1672560673/Portfolio/Capture1_xqivsd.jpg" alt="Sunset in the mountains"/>
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">Hashtag Technologies Website</div>
+      <p class="text-gray-700 text-base">
+      Complete website demonstrating the services and goods offered at Hashtag Technologies Limited.   </p>
+    </div>
+  <a href="http://hashtagtech.co.ke/" class="w-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+    <span>Visit Site</span>
+  </a>
+</div>
+<div class="w-full lg:w-1/3 rounded overflow-hidden shadow-lg m-10">
+  <img class="w-full" src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1672560674/Portfolio/Capture4_gyt5sw.jpg" alt="Sunset in the mountains"/>
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">My Portfolio</div>
+      <p class="text-gray-700 text-base">
+      Complete website demonstrating the services and goods offered at Hashtag Technologies Limited.   </p>
+    </div>
+  <a href="http://hashtagtech.co.ke/" class="w-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+    <span>Visit Site</span>
+  </a>
+</div>
+
+</div>
+                      </div></section>
+                  </div>
+                </div>
+              </div>
+            </div> : <div></div>}
         </div>
       </div>
     </div>
